1a1fd153d43a89361860943d1cd2009d2a6eba95
warrockhali <warrockhali@gmail.com>
2026-04-05 16:40:47 +0900

    [Silver III] Title: 소수 구하기, Time: 700 ms, Memory: 31972 KB -BaekjoonHub

---
+++ Java/백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = new boolean[n + 1];
+
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+
+            if (isPrime[i]) {
+
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+
+            }
+        }
+
+        for (int i = m; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
