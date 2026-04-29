9ba0243a168144dcf492861176710ab2fe33f33b
warrockhali <warrockhali@gmail.com>
2026-04-29 17:04:49 +0900

    [level 2] Title: 타겟 넘버, Time: 4.12 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
+    }
+
+    private int dfs(int[] numbers, int target, int index, int sum) {
+
+        if (index == numbers.length) {
+
+            if (sum == target) {
+                return 1;
+            }else {
+                return 0;
+            }
+
+        }
+
+        return dfs(numbers, target, index + 1, sum + numbers[index]) +
+                dfs(numbers, target, index + 1, sum - numbers[index]);
+    }
+}
