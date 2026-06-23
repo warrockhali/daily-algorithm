3c27bd9264ed3fb3ec246db9f1a44b68c6eb1474
warrockhali <warrockhali@gmail.com>
2026-06-23 12:11:29 +0900

    Time: 1 ms (99.87%), Space: 46.4 MB (95.73%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> results = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                results.add("FizzBuzz");
+            }else if (i % 3 == 0) {
+                results.add("Fizz");
+            }else if (i % 5 == 0) {
+                results.add("Buzz");
+            }else {
+                results.add(String.valueOf(i));
+            }
+
+        }
+
+        return results;
+    }
+}
