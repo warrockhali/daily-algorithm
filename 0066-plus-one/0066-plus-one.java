703d5d67baaf8a0b3199f81ed831b8187c435fd9
warrockhali <warrockhali@gmail.com>
2026-07-19 20:30:01 +0900

    Time: 0 ms (100%), Space: 43.3 MB (80.03%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }else if (digits[i] == 9) {
+                digits[i] = 0;
+            }
+
+        }
+
+        int[] results = new int[digits.length + 1];
+        results[0] = 1;
+
+        return results;
+    }
+}
